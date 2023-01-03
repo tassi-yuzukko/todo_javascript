d11ea5819187e440718c3e4235ba3583bdd564fb
tashima <tassi.yuzukko@gmail.com>
2023-01-03 03:45:54 +0000

    uptedate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+function onClickAdd(){
+  alert();
+}
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
